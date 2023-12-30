ec3e9ae50725b585d988d2abdb3c3e1e572bbfe5
Igor Świerczyński <swierczynskigor@gmail.com>
2023-12-30 16:51:54 +0100

    remove console logs

--- src/components/molecules/ShowImages/ShowImages.tsx
+++ src/components/molecules/ShowImages/ShowImages.tsx
@@ -20,8 +20,6 @@ export const ShowImages = ({
   onDelete,
   onImageClick,
 }: ShowImagesI) => {
-  console.log(linkedImages);
-
   return (
     <div className="images-container">
       {images
--- src/components/organizm/UploadContainer/UploadContainer.tsx
+++ src/components/organizm/UploadContainer/UploadContainer.tsx
@@ -63,7 +63,6 @@ export const UploadContainer = ({ url, title }: UploadContainerI) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  console.log(uploadedImages);
   return (
     <>
       <h3>{title}</h3>
