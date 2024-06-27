81ed9822b843222af73c44ef034578a1ccbafe27
Igor Świerczyński <swierczynskigor@gmail.com>
2024-06-27 20:39:07 +0200

    aaaaaaaaa

--- src/utils/getImagePath.ts
+++ src/utils/getImagePath.ts
@@ -5,7 +5,11 @@ export const getImagePath = (
   image: PhotoI,
   type: "thumb" | "fullsize" = "fullsize"
 ) => {
-  if (type === "thumb") return `${storageBucketUrl}/${image.thumbPath}`;
-  else if (type === "fullsize") return `${storageBucketUrl}/${image.path}`;
+  // if (type === "thumb") return `${storageBucketUrl}/${image.thumbPath}`;
+  // else if (type === "fullsize") return `${storageBucketUrl}/${image.path}`;
+  if (type === "thumb")
+    return `${storageBucketUrl}/api/thumb/${image.album}/${image.id}.${image.extension}`;
+  else if (type === "fullsize")
+    return `${storageBucketUrl}/api/image/${image.album}/${image.id}.${image.extension}`;
   return "";
 };
--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:80",
+        target: "http://localhost:8000",
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
